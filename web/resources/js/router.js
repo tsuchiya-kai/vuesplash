e28faff6d7dec5db83f744b0c342707e56274fdc
tsuchiya-kai <yukaigangan@gmail.com>
2020-08-04 00:41:48 +0900

    url直打ちでログインページに飛べないようにする

--- web/resources/js/router.js
+++ web/resources/js/router.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import PhotoList from "./pages/PhotoList.vue";
 import Login from "./pages/Login.vue";
+import store from "./store";
 
 // VueRouterプラグインの使用を宣言
 Vue.use(VueRouter);
@@ -14,7 +15,14 @@ const routes = [
   },
   {
     path: "/login",
-    component: Login
+    component: Login,
+    beforeEnter(to, from, next) {
+      if (store.getters["auth/check"]) {
+        next("/");
+      } else {
+        next();
+      }
+    }
   }
 ];
 
