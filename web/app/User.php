800415578a9d2dc0c62f1f36229729064816e227
tsuchiya-kai <yukaigangan@gmail.com>
2020-08-09 22:21:49 +0900

    写真一覧APIの実装

--- web/app/User.php
+++ web/app/User.php
@@ -18,13 +18,8 @@ class User extends Authenticatable
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
+    protected $visible = [
+        'name',
     ];
 
     /**
